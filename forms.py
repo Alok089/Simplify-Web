647aa4da1d9365bd1db6c0dc5b9214c3e0bcd55d
Alok089 <mehta.alok89@gmail.com>
2023-04-25 15:19:22 +0530

    CSRF Token Added the CSRF token to my login page

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, URL, Email
 from flask_ckeditor import CKEditorField
+from wtforms.csrf.session import SessionCSRF
+from datetime import timedelta
 
 ##WTForm
 class CreatePostForm(FlaskForm):
@@ -20,6 +22,11 @@ class RegisterForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email("Please enter a valid email")])
     password = PasswordField(label='Password',  validators=[DataRequired()])
     submit = SubmitField(label='Register')
+    class Meta:
+        csrf = True
+        csrf_class = SessionCSRF
+        csrf_secret = b'EPj00jpfj8Gx1SfdgsdsdafjQfnQ9DJYe0Ym'
+        csrf_time_limit = timedelta(minutes=20)
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired()])
