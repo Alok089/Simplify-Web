57af44072bf7fd1e7dda89132333bf17a71cda6d
Alok089 <mehta.alok89@gmail.com>
2023-05-01 12:58:03 +0530

    Adding FB login Adding the FB login button to get the user access token

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask_login import login_user, LoginManager, login_required, current_user, 
 from forms import CreatePostForm, LoginForm, CommentForm, RegisterForm
 from flask_gravatar import Gravatar
 from database import Comments, BlogPost, User, db
+from flask import jsonify
 # from flask_wtf.csrf import CSRFProtect
 # import random
 
@@ -187,6 +188,11 @@ def privacy():
 def facebook():
     return render_template("facebooklogin.html")
 
+@app.route('/fb_auth',methods=['POST'])
+def pass_val():
+    name=request.args.get('value')
+    print('name',name)
+    return jsonify({'reply':'success'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
