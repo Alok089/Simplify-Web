3c2f42ef2698b19d67b974f21549dd227ae516e7
Alok089 <mehta.alok89@gmail.com>
2023-05-02 16:53:48 +0530

    Adding FB Pages Adding Fb pages post login

--- integrations.py
+++ integrations.py
@@ -14,12 +14,28 @@ class Facebook:
         # TODO 1: Get userid and user access token:
         fb_page_list = []
         for page in fb_page_api['data']:
-            # insights = graph.get_object(f"{page['access_token']}/insights/page_impressions_unique")
+            page_graph = facebook.GraphAPI(access_token=page['access_token'], version="2.12")
+            insights = page_graph.get_object(f"{page['id']}/insights/page_impressions_unique")
+            listed_metrics = []
+            for metric in insights['data']:
+                if f'{metric["name"]}_{metric["period"]}' == 'page_impressions_unique_day':
+                    metric_title = "Unique Page Impressions Per Day"
+                elif f'{metric["name"]}_{metric["period"]}' == 'page_impressions_unique_week':
+                    metric_title = "Unique Page Impressions Per Week"
+                elif f'{metric["name"]}_{metric["period"]}' == 'page_impressions_unique_days_28':
+                    metric_title = "Unique Page Impressions Every 28 Days"
+                else:
+                    metric_title = "New Metric"
+                listed_metrics.append({metric_title: metric["values"][1]['value']})
             fb_page_list.append({'Name': page['name'],
                                  'AccessToken': page['access_token'],
-                                 # 'Insights': insights
+                                 'Insights': listed_metrics
                                  })
         return {"data": fb_page_list}
 
-# test = Facebook("EABQUuk5VBgUBAK00JRQjkl9rkmTs09TjmzYrd9N1PIfeDs3dB4gLIZBQnKCs93OWpMKV042vooFTYAWsqUrko7cMc049uiggDCQBowlAf0JYaBpaqeMN7ulznAENQj4HoNMTaITZCQKRdLgoL5ylEizPszTC2j90OSeFPzx36eKa0FGIdAUWFMqFdbyMCFZB7ZAxAeZA1M8KmS3xLQcoZC97vZCnrrEFM4ZD")
-# page_list = test.get_pages()
+test = Facebook("EABQUuk5VBgUBAD5PNQcSohRrwqoNZCrrLSdMhAO5lzaV9DxbAYPZCc1oyRaaaWqdLR3jKGhMrBZCQyk0SSA4rrVMQPZCQ12bc0sr6C1IZB5EcTcAVRPI4PKNZB4UiUyy7inLIYKIugZBZCWnlVVZA28nVLcd3uvrL0IxnBaeMoXixUZBmXvFKmAWVm2XZAIBAnZBXS3jEzmZAjMe4j83HDOxYSLQZCLmGLDjQf4r8ZD")
+page_list = test.get_pages()
+for metric in page_list['data']:
+    for dict in metric['Insights']:
+        for key, value in dict.items():
+            print(f'{key} - {value}')
