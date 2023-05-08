e6c2c3234d024dda50327c74b039f102fd981132
Alok089 <mehta.alok89@gmail.com>
2023-05-08 16:16:36 +0530

    Adding FB Pages Adding Fb pages post login

--- integrations.py
+++ integrations.py
@@ -14,23 +14,22 @@ class Facebook:
         # TODO 1: Get userid and user access token:
         fb_page_list = []
         for page in fb_page_api['data']:
-            page_token = page['access_token']
-            page_graph = facebook.GraphAPI(access_token=page_token, version="2.12")
-            insights = page_graph.get_object(f"{page['id']}/insights/page_impressions")
-            listed_metrics = {}
-            for metric in insights['data']:
-                if f'{metric["name"]}_{metric["period"]}' == 'page_impressions_day':
-                    metric_title = "Page Impressions Per Day"
-                elif f'{metric["name"]}_{metric["period"]}' == 'page_impressions_week':
-                    metric_title = "Page Impressions Per Week"
-                elif f'{metric["name"]}_{metric["period"]}' == 'page_impressions_days_28':
-                    metric_title = "Page Impressions Every 28 Days"
-                else:
-                    metric_title = "New Metric"
-                listed_metrics[metric_title] = metric["values"][1]['value']
-            fb_page_list.append({'Name': page['name'],
-                                 'AccessToken': page['access_token'],
-                                 'Insights': listed_metrics
-                                 })
+            if 'access_token' in page:
+                page_graph = facebook.GraphAPI(access_token=page['access_token'], version="2.12")
+                insights = page_graph.get_object(f"{page['id']}/insights/page_impressions")
+                listed_metrics = {}
+                for metric in insights['data']:
+                    if f'{metric["name"]}_{metric["period"]}' == 'page_impressions_day':
+                        metric_title = "Page Impressions Per Day"
+                    elif f'{metric["name"]}_{metric["period"]}' == 'page_impressions_week':
+                        metric_title = "Page Impressions Per Week"
+                    elif f'{metric["name"]}_{metric["period"]}' == 'page_impressions_days_28':
+                        metric_title = "Page Impressions Every 28 Days"
+                    else:
+                        metric_title = "New Metric"
+                    listed_metrics[metric_title] = metric["values"][1]['value']
+                fb_page_list.append({'Name': page['name'],
+                                     'AccessToken': page['access_token'],
+                                     'Insights': listed_metrics
+                                     })
         return {"data": fb_page_list}
-
