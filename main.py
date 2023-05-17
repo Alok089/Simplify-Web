bcabd410203629634c5e7dde0b0be2285ab60f68
Alok089 <mehta.alok89@gmail.com>
2023-05-17 18:20:13 +0530

    AI View Model output using UI

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ def fb_posts(page_name):
                     db.session.commit()
     store_data(fb.all_videos)
     store_data(fb.all_reels)
-    AI.teach_model('Entertainment', 'Celebrities', 'name','Anil Kapoor','Title')
+    AI.teach_model('Entertainment', 'Celebrities', 'name','Dalip Tahil','Title')
     all_videos = AI.build_default_view()
     return render_template("post_details.html", page_name = page_name, videos=all_videos, reels=fb.all_reels)
 
